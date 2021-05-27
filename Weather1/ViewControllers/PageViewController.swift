215ad9d61d5e56bb09624e6ba9e9dba152517549
Artem Bazhanov <abazhanov@yahoo.com>
2021-05-27 22:56:19 +0300

    Test of pageViewController

---
+++ Weather1/ViewControllers/PageViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PageViewController.swift
+//  Weather1
+//
+//  Created by Artem Bazhanov on 27.05.2021.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
+ 
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("PageViewController")
+        self.dataSource = self
+        
+        if let vc = returnPageViewController(for: 1) {
+            setViewControllers([vc], direction: .forward, animated: false, completion: nil)
+        }
+        
+        
+        
+//        setViewControllers([returnPageViewController(for: 1)], direction: .forward, animated: false) { (bool) in
+//            <#code#>
+//        }
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return returnPageViewController(for: 1)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return returnPageViewController(for: 1)
+    }
+
+    func returnPageViewController(for index: Int) -> MainViewController? {
+        let vc = storyboard?.instantiateViewController(withIdentifier: "mainVC") as! MainViewController
+        print("VC = ", vc)
+        return vc
+    }
+}
